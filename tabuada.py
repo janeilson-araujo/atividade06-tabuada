0d7bb89544acfa9839b226043b65ef0f56cc7347
janeilson-araujo <janeilsonpaulino013@gmail.com>
2023-10-17 10:35:09 -0300

    atividade 06 tabuada python

---
+++ tabuada.py
@@ -0,0 +1,14 @@
+# Digite um numero interio e veja sua tabuada
+
+print("tabuada de qualquer numero inteiro")
+numero = int(input("digite o numero"))
+print(numero*1)
+print(numero*2)
+print(numero*3)
+print(numero*4)
+print(numero*5)
+print(numero*6)
+print(numero*7)
+print(numero*8)
+print(numero*9)
+print(numero*10)
